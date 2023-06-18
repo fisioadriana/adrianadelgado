7b36d1a847f882a599e3850a1a5ea1b23c7f4413
fisioadriana <136936531+fisioadriana@users.noreply.github.com>
2023-06-18 13:20:50 -0500

    Update aside.php
    eliminar acide

--- templates/aside.php
+++ templates/aside.php
@@ -1,8 +1,1 @@
-<aside>
-  <h3>Publicidad</h3>
-  <div class="anuncios">
-    <img src="img/stretcher.jpg" width="400" class="img-fluid rounded float-end" alt="">
-    <img src="img/physiotherapy.jpg" width="400" class="img-fluid rounded float-end" alt="">
-  </div>
 
-</aside>
